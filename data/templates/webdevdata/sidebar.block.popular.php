fb6a31c1d5d6eaa11e4a6d1fc104506aed8c7fe0
David Newton <david@davidnewton.ca>
2013-11-21 15:28:58 -0500

    Make popular tags universal

--- data/templates/webdevdata/sidebar.block.popular.php
+++ data/templates/webdevdata/sidebar.block.popular.php
@@ -2,12 +2,9 @@
 /* Service creation: only useful services are created */
 $b2tservice =SemanticScuttle_Service_Factory::get('Bookmark2Tag');
 
-$logged_on_userid = $userservice->getCurrentUserId();
-if(!isset($user) || !isset($userid) || $logged_on_userid === false) {
-	$user = '';
-	$userid = NULL;
-	$logged_on_userid = NULL;
-}
+$user = '';
+$userid = NULL;
+$logged_on_userid = NULL;
 
 $popularTags =& $b2tservice->getPopularTags($userid, $popCount, $logged_on_userid);
 $popularTags =& $b2tservice->tagCloud($popularTags, 5, 90, 225, 'alphabet_asc');
