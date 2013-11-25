e9ee6730479bdc5352a0c875d9a2d4f6b7d1cca0
David Newton <david@davidnewton.ca>
2013-11-25 14:30:16 -0500

    oops. viewport.

--- data/templates/webdevdata/top.inc.php
+++ data/templates/webdevdata/top.inc.php
@@ -4,6 +4,7 @@
 
 	<head>
 		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<title><?php echo filter($GLOBALS['sitename'] . (isset($pagetitle) ? ' » ' . $pagetitle : '')); ?></title>
 
